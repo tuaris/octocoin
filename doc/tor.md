9aad8ce006e3c6c17d7bc760889bdfa2fdd1f059
Daniel Morante <daniel@morante.net>
2015-09-12 22:12:35 -0400

    Update port numbers in documentation

--- doc/tor.md
+++ doc/tor.md
@@ -42,11 +42,11 @@ reachable from the Tor network. Add these lines to your /etc/tor/torrc (or equiv
 config file):
 
 	HiddenServiceDir /var/lib/tor/octocoin-service/
-	HiddenServicePort 9333 127.0.0.1:9333
-	HiddenServicePort 19333 127.0.0.1:19333
+	HiddenServicePort 22889 127.0.0.1:22889
+	HiddenServicePort 32889 127.0.0.1:32889
 
 The directory can be different of course, but (both) port numbers should be equal to
-your octocoind's P2P listen port (9333 by default).
+your octocoind's P2P listen port (22889 by default).
 
 	-externalip=X   You can tell octocoin about its publicly reachable address using
 	                this option, and this can be a .onion address. Given the above
@@ -76,7 +76,7 @@ specify:
 
 	./octocoind ... -discover
 
-and open port 9333 on your firewall (or use -upnp).
+and open port 22889 on your firewall (or use -upnp).
 
 If you only want to use Tor to reach onion addresses, but not use it as a proxy
 for normal IPv4/IPv6 communication, use:
--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -60,15 +60,15 @@ BOOST_AUTO_TEST_CASE(netbase_splithost)
     BOOST_CHECK(TestSplitHost("www.bitcoin.org:80", "www.bitcoin.org", 80));
     BOOST_CHECK(TestSplitHost("[www.bitcoin.org]:80", "www.bitcoin.org", 80));
     BOOST_CHECK(TestSplitHost("127.0.0.1", "127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("127.0.0.1:9333", "127.0.0.1", 9333));
+    BOOST_CHECK(TestSplitHost("127.0.0.1:22889", "127.0.0.1", 22889));
     BOOST_CHECK(TestSplitHost("[127.0.0.1]", "127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("[127.0.0.1]:9333", "127.0.0.1", 9333));
+    BOOST_CHECK(TestSplitHost("[127.0.0.1]:22889", "127.0.0.1", 22889));
     BOOST_CHECK(TestSplitHost("::ffff:127.0.0.1", "::ffff:127.0.0.1", -1));
-    BOOST_CHECK(TestSplitHost("[::ffff:127.0.0.1]:9333", "::ffff:127.0.0.1", 9333));
-    BOOST_CHECK(TestSplitHost("[::]:9333", "::", 9333));
-    BOOST_CHECK(TestSplitHost("::9333", "::9333", -1));
-    BOOST_CHECK(TestSplitHost(":9333", "", 9333));
-    BOOST_CHECK(TestSplitHost("[]:9333", "", 9333));
+    BOOST_CHECK(TestSplitHost("[::ffff:127.0.0.1]:22889", "::ffff:127.0.0.1", 22889));
+    BOOST_CHECK(TestSplitHost("[::]:22889", "::", 22889));
+    BOOST_CHECK(TestSplitHost("::22889", "::22889", -1));
+    BOOST_CHECK(TestSplitHost(":22889", "", 22889));
+    BOOST_CHECK(TestSplitHost("[]:22889", "", 22889));
     BOOST_CHECK(TestSplitHost("", "", -1));
 }
 
@@ -83,10 +83,10 @@ bool static TestParse(string src, string canon)
 BOOST_AUTO_TEST_CASE(netbase_lookupnumeric)
 {
     BOOST_CHECK(TestParse("127.0.0.1", "127.0.0.1:65535"));
-    BOOST_CHECK(TestParse("127.0.0.1:9333", "127.0.0.1:9333"));
+    BOOST_CHECK(TestParse("127.0.0.1:22889", "127.0.0.1:22889"));
     BOOST_CHECK(TestParse("::ffff:127.0.0.1", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse("::", "[::]:65535"));
-    BOOST_CHECK(TestParse("[::]:9333", "[::]:9333"));
+    BOOST_CHECK(TestParse("[::]:22889", "[::]:22889"));
     BOOST_CHECK(TestParse("[127.0.0.1]", "127.0.0.1:65535"));
     BOOST_CHECK(TestParse(":::", ""));
 }
